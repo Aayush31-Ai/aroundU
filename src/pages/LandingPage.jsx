07968cd2a5f1ae8f04bb50366ff2aa344798ed36
Aayush Pal <ayushsurajpal123@gmail.com>
2026-01-14 10:46:58 +0530

    resolve file structure

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import Hero from "@/components/LandingPage/Hero";
 import HowItWorks from "@/components/LandingPage/HowItWorks";
 import MostBookedServices from "@/components/LandingPage/MostBookedServices";
 import React from "react";
-import WhyChooseUs from "./WhyChooseUs";
+import WhyChooseUs from "@/components/LandingPage/WhyChooseUs";
 import Footer from "@/components/Footer/Footer";
 
 const LandingPage = () => {
